99dee7827b4f9125d0ff1ddabc22b0801b5ce476
notexplosive <thenotexplosive@gmail.com>
2023-07-06 00:31:00 -0700

    added build pipeline

--- relase_build.bat
+++ relase_build.bat
@@ -1,2 +1,4 @@
 neato monogame-release-build .\GMTK23\GMTK23.csproj ".build"
+powershell -command "ls .build | where name -like *.pdb | remove-item"
+neato publish-itch ".build" "notexplosive" "gmtk2023" "windows"
 explorer ".build"
