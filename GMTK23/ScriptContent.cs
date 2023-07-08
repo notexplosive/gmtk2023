a883401bfcd021e87c700e0a8c8c7fe208fb7cca
notexplosive <thenotexplosive@gmail.com>
2023-07-08 10:18:41 -0700

    add music

--- GMTK23/ScriptContent.cs
+++ GMTK23/ScriptContent.cs
@@ -13,12 +13,17 @@ public static class ScriptContent
 
     public static IEnumerable<Wave> Summons(Game game)
     {
+        yield return BasicFormation(game);
+    }
+
+    private static Wave BasicFormation(Game game)
+    {
         var worldBounds = game.World.Bounds;
         var worldBoundsInset = worldBounds.Inflated(-20, -20);
         var worldBoundsOutset = worldBounds.Inflated(64, 64);
-        var summon = new Wave(game, new WaveStats(1f));
+        var wave = new Wave(game, new WaveStats(1f));
 
-        var main = summon.AddChoreoid(ScriptContent.BasicEnemy);
+        var main = wave.AddChoreoid(ScriptContent.BasicEnemy);
 
         var rightMiddleSpawn = new Vector2(worldBoundsOutset.Right, worldBounds.Center.Y);
         var leftMiddleSpawn = new Vector2(worldBoundsOutset.Left, worldBounds.Center.Y);
@@ -30,7 +35,7 @@ public static class ScriptContent
             .AddMoveToFastX(new Vector2(worldBounds.Width * 7 / 8f, worldBoundsInset.Bottom), 0.5f)
             .AddWait(0.1f);
 
-        main.AddWaitEvent(0.1f);
+        main.AddWaitEvent(0.2f);
         main.AddSpawnEvent(leftMiddleSpawn)
             .AddMoveToFastX(new Vector2(worldBounds.Width / 3, worldBoundsInset.Bottom), 0.5f)
             .AddWait(0.1f);
@@ -38,6 +43,6 @@ public static class ScriptContent
             .AddMoveToFastX(new Vector2(worldBounds.Width * 2 / 3f, worldBoundsInset.Bottom), 0.5f)
             .AddWait(0.1f);
 
-        yield return summon;
+        return wave;
     }
 }
